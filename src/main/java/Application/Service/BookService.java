e933a9fc58a4f75ce695d4211b86633c30ddd11f
Chad Rotruck <crotruck@gmail.com>
2023-03-04 20:13:49 +0000

    Done

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,7 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+        return this.bookDAO.getAllBooks();
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +51,19 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
-        return null;
+        Book book2 = this.bookDAO.getBookByIsbn(book.getIsbn());
+        if (book2 == null){
+            return this.bookDAO.insertBook(book);
+        } else {
+            return null;
+        }
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return this.bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
